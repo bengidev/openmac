bf72b4e623a2be60827ca4647ce174e06be4f39d
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-04-29 16:30:17 +0700

    test(unit): add baseline OpenMac tests

---
+++ OpenMacTests/OpenMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenMacTests.swift
+//  OpenMacTests
+//
+//  Created by Bambang Tri Rahmat Doni on 29/04/26.
+//
+
+import Testing
+@testable import OpenMac
+
+struct OpenMacTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+}
